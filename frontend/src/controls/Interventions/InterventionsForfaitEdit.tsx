269f66b0b3aec2b6d0b2b46a7ad8a6f37efd167e
jlemon100p <julien.lemonnier@u-bordeaux.fr>
2026-04-15 15:54:42 +0200

    refacto(front): décomposition des gros composants (InterventionsForfaitEdit)

--- frontend/src/controls/Interventions/InterventionsForfaitEdit.tsx
+++ frontend/src/controls/Interventions/InterventionsForfaitEdit.tsx
@@ -10,35 +10,13 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import "../../routes/administration/Administration.scss";
 import { QK_INTERVENTIONS_FORFAIT } from "../../api/queryKeys";
-import {
-   Alert,
-   Button,
-   Card,
-   Col,
-   Drawer,
-   Dropdown,
-   Form,
-   InputNumber,
-   MenuProps,
-   Popconfirm,
-   Row,
-   Select,
-   Space,
-} from "antd";
+import { Button, Card, Drawer, Dropdown, Form, Popconfirm, Space } from "antd";
 import { NB_MAX_ITEMS_PER_PAGE } from "../../constants";
 import { useApi } from "../../context/api/ApiProvider";
-import TypeEvenementItem from "../Items/TypeEvenementItem";
-import PeriodeRhItem from "../Items/PeriodeRhItem";
-import UtilisateurFormItemSelect from "../Forms/UtilisateurFormItemSelect";
-import { RoleValues } from "../../lib/Utilisateur";
-import { DeleteOutlined, DownOutlined, ExclamationOutlined, SaveOutlined } from "@ant-design/icons";
-import { useAuth } from "../../auth/AuthProvider";
-import dayjs from "dayjs";
-import GestionnaireItem from "../Items/GestionnaireItem";
-import { TabEvenementParticipantsBeneficiaires } from "../TabsContent/SubTabs/TabEvenementParticipantsBeneficiaires";
+import { DeleteOutlined, DownOutlined, SaveOutlined } from "@ant-design/icons";
 import { IInterventionForfait } from "../../api/ApiTypeHelpers";
-
 import { UseStateDispatch } from "../../utils/utils";
+import { InterventionsForfaitForm } from "./InterventionsForfaitForm";
 
 interface IInterventionsForfaitEditProps {
    editedItem: Partial<IInterventionForfait>;
@@ -47,96 +25,51 @@ interface IInterventionsForfaitEditProps {
 
 /**
  * Edit or add an intervention forfait
- *
- * @param {Object} props - The component properties
- * @param {Object} props.editedItem - The item being edited
- * @param {Function} props.setEditedItem - The function to set the edited item
- * @returns {ReactElement} - The rendered component
  */
 export default function InterventionsForfaitEdit({
    editedItem,
    setEditedItem,
 }: IInterventionsForfaitEditProps): ReactElement {
-   const user = useAuth().user;
    const [form] = Form.useForm();
    const [beneficiairesModifies, setBeneficiairesModifies] = useState(false);
-   const { data: periodes, isFetching: isFetchingPeriodes } = useApi().useGetCollectionPaginated({
+
+   const { data: periodes } = useApi().useGetCollectionPaginated({
       path: "/periodes",
       page: 1,
       itemsPerPage: NB_MAX_ITEMS_PER_PAGE,
    });
-   const { data: typesEvenements, isFetching: isFetchingTypesEvenements } =
-      useApi().useGetCollectionPaginated({
-         path: "/types_evenements",
-         page: 1,
-         itemsPerPage: NB_MAX_ITEMS_PER_PAGE,
-         query: {
-            forfait: true,
-         },
-      });
 
    // --- Mutations
    const createInterventionForfait = useApi().usePost({
       path: "/interventions_forfait",
       invalidationQueryKeys: [QK_INTERVENTIONS_FORFAIT],
-      onSuccess: () => {
-         setEditedItem(undefined);
-      },
+      onSuccess: () => setEditedItem(undefined),
    });
 
    const updateInterventionForfait = useApi().usePatch({
       path: "/interventions_forfait/{id}",
       invalidationQueryKeys: [QK_INTERVENTIONS_FORFAIT],
-      onSuccess: () => {
-         setEditedItem(undefined);
-      },
+      onSuccess: () => setEditedItem(undefined),
    });
 
    const deleteInterventionForfait = useApi().useDelete({
       path: "/interventions_forfait/{id}",
       invalidationQueryKeys: [QK_INTERVENTIONS_FORFAIT],
-      onSuccess: () => {
-         setEditedItem(undefined);
-      },
+      onSuccess: () => setEditedItem(undefined),
    });
 
-   const itemsMenu: MenuProps["items"] = [
-      {
-         key: "supprimer",
-         label: (
-            <Popconfirm
-               title="Supprimer cette intervention ?"
-               okButtonProps={{ danger: true }}
-               okText="Oui, supprimer"
-               cancelText="Non"
-               onConfirm={() => {
-                  deleteInterventionForfait.mutate({
-                     "@id": editedItem["@id"] as string,
-                  });
-               }}
-            >
-               Supprimer
-            </Popconfirm>
-         ),
-         icon: <DeleteOutlined />,
-         danger: true,
-      },
-   ];
+   const isEditable =
+      !editedItem.periode ||
+      !periodes?.items?.find((p) => p["@id"] === editedItem.periode)?.envoyee;
 
    const save = (values: IInterventionForfait) => {
+      const data = { ...values, heures: values.heures.toString() };
       if (editedItem["@id"]) {
-         updateInterventionForfait.mutate({ data: values, "@id": editedItem["@id"] });
+         updateInterventionForfait.mutate({ data, "@id": editedItem["@id"] });
       } else {
-         createInterventionForfait.mutate({ data: values });
+         createInterventionForfait.mutate({ data });
       }
    };
-
-   function editable() {
-      return (
-         !editedItem.periode ||
-         !periodes?.items?.find((p) => p["@id"] === editedItem.periode)?.envoyee
-      );
-   }
 
    // Sync form with editedItem
    useEffect(() => {
@@ -149,194 +82,82 @@ export default function InterventionsForfaitEdit({
          className="bg-light-grey"
          open
          title={
-            !editable()
+            !isEditable
                ? "Consulter une intervention au forfait"
                : editedItem["@id"]
                  ? "Modifier une intervention au forfait"
                  : "Ajouter une intervention au forfait"
          }
-         onClose={() => {
-            setEditedItem(undefined);
-         }}
+         onClose={() => setEditedItem(undefined)}
          size="large"
       >
-         <Form<IInterventionForfait>
-            layout="vertical"
-            form={form}
-            onFinish={(values) => {
-               save({ ...(values as IInterventionForfait), heures: values.heures.toString() });
-            }}
-            initialValues={{
-               type: typesEvenements?.items[0]["@id"],
-            }}
-         >
-            <Card
-               title="Intervention au forfait"
-               actions={[
-                  editable() && <Button onClick={() => setEditedItem(undefined)}>Annuler</Button>,
-                  (editable() || beneficiairesModifies) && (
-                     <Button type="primary" icon={<SaveOutlined />} htmlType="submit">
+         <Card
+            title="Intervention au forfait"
+            actions={
+               [
+                  isEditable && <Button onClick={() => setEditedItem(undefined)}>Annuler</Button>,
+                  (isEditable || beneficiairesModifies) && (
+                     <Button
+                        type="primary"
+                        icon={<SaveOutlined />}
+                        onClick={() => form.submit()}
+                        loading={
+                           createInterventionForfait.isPending ||
+                           updateInterventionForfait.isPending
+                        }
+                     >
                         Enregistrer
                      </Button>
                   ),
-                  !editable() && (
-                     <Button disabled={false} onClick={() => setEditedItem(undefined)}>
-                        Fermer
-                     </Button>
-                  ),
-               ].filter((a) => a !== false)}
-               extra={
-                  editedItem["@id"] && editable() ? (
-                     <Dropdown menu={{ items: itemsMenu }}>
-                        <Space>
-                           <Button className="no-hover" type="text" icon={<DownOutlined />}>
-                              Options
-                           </Button>
-                        </Space>
-                     </Dropdown>
-                  ) : undefined
-               }
-            >
-               <Form.Item
-                  label="Période"
-                  name="periode"
-                  required
-                  rules={[{ required: true, message: "Veuillez sélectionner une période" }]}
-               >
-                  <Select
-                     value={editedItem.periode}
-                     onChange={(value) => {
-                        setEditedItem({ ...editedItem, periode: value });
+                  !isEditable && <Button onClick={() => setEditedItem(undefined)}>Fermer</Button>,
+               ].filter(Boolean) as React.ReactNode[]
+            }
+            extra={
+               editedItem["@id"] && isEditable ? (
+                  <Dropdown
+                     menu={{
+                        items: [
+                           {
+                              key: "supprimer",
+                              label: (
+                                 <Popconfirm
+                                    title="Supprimer cette intervention ?"
+                                    okButtonProps={{ danger: true }}
+                                    okText="Oui, supprimer"
+                                    cancelText="Non"
+                                    onConfirm={() =>
+                                       deleteInterventionForfait.mutate({
+                                          "@id": editedItem["@id"] as string,
+                                       })
+                                    }
+                                 >
+                                    Supprimer
+                                 </Popconfirm>
+                              ),
+                              icon: <DeleteOutlined />,
+                              danger: true,
+                           },
+                        ],
                      }}
-                     className="w-100"
-                     loading={isFetchingPeriodes}
-                     allowClear
-                     disabled={!editable()}
                   >
-                     {periodes?.items
-                        .filter((p) => !p.envoyee || !editable())
-                        .filter((p) => user?.isAdmin || dayjs(p.butoir).isAfter(dayjs()))
-                        .map((periode) => (
-                           <Select.Option key={periode["@id"]} value={periode["@id"]}>
-                              <PeriodeRhItem periode={periode} />
-                           </Select.Option>
-                        ))}
-                  </Select>
-               </Form.Item>
-               <Form.Item
-                  label="Intervenant"
-                  name="intervenant"
-                  rules={[{ required: true, message: "Veuillez sélectionner un intervenant" }]}
-                  required
-               >
-                  <UtilisateurFormItemSelect
-                     className="w-100"
-                     onSelect={(value) => {
-                        setEditedItem({ ...editedItem, intervenant: value });
-                     }}
-                     placeholder="Rechercher un intervenant"
-                     roleUtilisateur={RoleValues.ROLE_INTERVENANT}
-                     disabled={!editable()}
-                  />
-               </Form.Item>
-               <Form.Item
-                  label="Catégorie d'événement"
-                  required
-                  name="type"
-                  rules={[
-                     {
-                        required: true,
-                        message: "Veuillez sélectionner une catégorie d'événement",
-                     },
-                  ]}
-               >
-                  <Select
-                     value={editedItem.type}
-                     onChange={(value) => {
-                        setEditedItem({ ...editedItem, type: value });
-                     }}
-                     className="w-100"
-                     loading={isFetchingTypesEvenements}
-                     allowClear
-                     disabled={!editable()}
-                  >
-                     {typesEvenements?.items.map((typeEvenement) => (
-                        <Select.Option key={typeEvenement["@id"]} value={typeEvenement["@id"]}>
-                           <TypeEvenementItem typeEvenement={typeEvenement} showAvatar={false} />
-                        </Select.Option>
-                     ))}
-                  </Select>
-               </Form.Item>
-               <Form.Item
-                  label="Durée"
-                  rules={[{ required: true, message: "Veuillez saisir une durée" }]}
-                  required
-                  name="heures"
-               >
-                  <InputNumber
-                     value={editedItem.heures}
-                     onChange={(value) => {
-                        setEditedItem({ ...editedItem, heures: value || "0" });
-                     }}
-                     className="w-100"
-                     placeholder="Durée en heures"
-                     min="0.00"
-                     step={0.5}
-                     precision={1}
-                     decimalSeparator=","
-                     disabled={!editable()}
-                  />
-               </Form.Item>
-               <TabEvenementParticipantsBeneficiaires
-                  evenement={editedItem}
-                  setEvenement={(v) => {
-                     setEditedItem({ ...editedItem, ...(v as IInterventionForfait) });
-                     setBeneficiairesModifies(true);
-                  }}
-               />
-               {(editedItem.beneficiaires || []).length === 0 && (
-                  <Alert
-                     type="warning"
-                     showIcon
-                     icon={<ExclamationOutlined />}
-                     title="Aucun bénéficiaire associé"
-                     description="Vous devez associer les bénéficiaires de cette intervention."
-                  />
-               )}
-               <Row className="mb-3">
-                  <Col span={24} className="legende mt-1">
-                     Créé le {dayjs(editedItem.dateCreation).format("DD/MM/YYYY")}
-                     {editedItem.utilisateurCreation && (
-                        <>
-                           {" "}
-                           par{" "}
-                           <GestionnaireItem
-                              gestionnaireId={editedItem.utilisateurCreation}
-                              showAvatar={false}
-                           />
-                        </>
-                     )}
-                     {editedItem.dateModification && (
-                        <>
-                           <br />
-                           Dernière modification le{" "}
-                           {dayjs(editedItem.dateModification).format("DD/MM/YYYY")}
-                           {editedItem.utilisateurModification && (
-                              <>
-                                 {" "}
-                                 par{" "}
-                                 <GestionnaireItem
-                                    gestionnaireId={editedItem.utilisateurModification}
-                                    showAvatar={false}
-                                 />
-                              </>
-                           )}
-                        </>
-                     )}
-                  </Col>
-               </Row>
-            </Card>
-         </Form>
+                     <Space>
+                        <Button className="no-hover" type="text" icon={<DownOutlined />}>
+                           Options
+                        </Button>
+                     </Space>
+                  </Dropdown>
+               ) : undefined
+            }
+         >
+            <InterventionsForfaitForm
+               form={form}
+               editedItem={editedItem}
+               setEditedItem={setEditedItem}
+               onFinish={save}
+               isEditable={isEditable}
+               setBeneficiairesModifies={setBeneficiairesModifies}
+            />
+         </Card>
       </Drawer>
    );
 }
