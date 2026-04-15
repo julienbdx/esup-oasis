d1cc87b07077f9da4fa6ccc9054d255464354a24
jlemon100p <julien.lemonnier@u-bordeaux.fr>
2026-04-15 19:38:55 +0200

    refacto(front): oubli craco.config.js (alias Typescript)

---
+++ frontend/craco.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@controls': path.resolve(__dirname, 'src/controls'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@controls/(.*)$': '<rootDir>/src/controls/$1',
+        '^@context/(.*)$': '<rootDir>/src/context/$1',
+        '^@lib/(.*)$': '<rootDir>/src/lib/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@api/(.*)$': '<rootDir>/src/api/$1',
+        '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+        '^@/(.*)$': '<rootDir>/src/$1'
+      }
+    }
+  }
+};
