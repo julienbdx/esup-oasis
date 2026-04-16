62718bf329ac83bdb420450726b3af09ec4bee27
jlemon100p <julien.lemonnier@u-bordeaux.fr>
2026-04-16 15:36:13 +0200

    refacto(deploy): suppression de react-inject-env

--- frontend/docker/docker-entrypoint.sh
+++ frontend/docker/docker-entrypoint.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-# Injecter les variables d'environnement dans le fichier env.js
-npx --loglevel error react-inject-env set --dir /usr/share/nginx/html
+# Supprimer les anciens fichiers env.*.js...
+rm -f /usr/share/nginx/html/env.*.js
+
+# Générer dynamiquement le fichier env-config.js avec toutes les variables REACT_APP_*
+TIMESTAMP=$(date +%s)
+ENV_FILE="/usr/share/nginx/html/env.${TIMESTAMP}.js"
+
+{
+  echo "window.env = {"
+  env | grep '^REACT_APP_' | while IFS='=' read -r key value; do
+    # Échapper les guillemets et backslashes dans la valeur
+    value=$(echo "$value" | sed 's/\\/\\\\/g; s/"/\\"/g')
+    echo "  $key: \"$value\","
+  done
+  echo "};"
+} > "$ENV_FILE"
+
+# Injecter le nom du fichier dans index.html (remplace /env.js ou /env.{timestamp}.js)
+sed -i "s|<script src=./env\.[0-9]*\.js.></script>|<script src='/env.${TIMESTAMP}.js'></script>|g" /usr/share/nginx/html/index.html
+sed -i "s|<script src=./env\.js.></script>|<script src='/env.${TIMESTAMP}.js'></script>|g" /usr/share/nginx/html/index.html
 
 # Générer la config Nginx avec les URLs réelles dans la Content-Security-Policy
 envsubst '${REACT_APP_API} ${REACT_APP_OAUTH_PROVIDER}' \
